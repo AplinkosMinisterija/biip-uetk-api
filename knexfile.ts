8072bbf30917cfe0966d8079323659d54fcceb99
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2023-08-11 15:46:59 +0300

    min/max pool

--- knexfile.ts
+++ knexfile.ts
@@ -1,19 +1,20 @@
 const { knexSnakeCaseMappers } = require('objection');
-const path = require("path")
+const path = require('path');
 
 if (!process.env.DB_CONNECTION) {
   throw new Error('No DB_CONNECTION env variable!');
 }
 
 const config = {
-  client: "pg",
+  client: 'pg',
   connection: process.env.DB_CONNECTION,
   migrations: {
     tableName: 'migrations',
-    directory: path.resolve('./database/migrations')
+    directory: path.resolve('./database/migrations'),
   },
+  pool: { min: 0, max: 2 },
   ...knexSnakeCaseMappers(),
 };
 
-export default config
-module.exports = config
+export default config;
+module.exports = config;
